d48292b6aff512d1cfda51d447a5553074687487
Truong Hy <truhy7@gmail.com>
2025-01-07 06:00:23 +0000

    Update tru_adxl345_ll.c

--- source/trulib/source/tru_adxl345_ll.c
+++ source/trulib/source/tru_adxl345_ll.c
@@ -1,13 +1,40 @@
+/*
+	MIT License
+
+	Copyright (c) 2024 Truong Hy
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+	Version: 20241222
+*/
+
 #include "tru_adxl345_ll.h"
 #include "tru_util_ll.h"
 #include "tru_c5soc_hps_ll.h"
 #include "tru_c5soc_hps_i2c_ll.h"
 
+// Setup HPS I2C0 controller
 void tru_adxl345_i2c_init(uint32_t l4_sp_clk_freq_hz, uint32_t i2c_dev_speed_khz, uint8_t mode10bit, uint16_t dev_addr){
 	// Release I2C0 from reset
 	TRU_HPS_RSTMGR_PERMODRST_REG->bits.i2c0 = 0;
 
-	// Temporary disable
+	// Temporary disable I2C0
 	TRU_HPS_I2C0_IC_ENABLE_REG->bits.enable = 0;
 
 	// Setup defaults
@@ -28,7 +55,7 @@ void tru_adxl345_i2c_init(uint32_t l4_sp_clk_freq_hz, uint32_t i2c_dev_speed_khz
 	//TRU_HPS_I2C0_IC_DMA_CR_REG->bits.rdmae = 1;
 	//TRU_HPS_I2C0_IC_DMA_CR_REG->bits.tdmae = 1;
 
-	// Enable
+	// Enable I2C0
 	TRU_HPS_I2C0_IC_ENABLE_REG->bits.enable = 1;
 
 	// Set desired speed and filter duration
@@ -71,7 +98,8 @@ void tru_adxl345_i2c_init(uint32_t l4_sp_clk_freq_hz, uint32_t i2c_dev_speed_khz
 	TRU_HPS_I2C0_IC_TAR_REG->bits.ic_tar = dev_addr;
 }
 
-// Reads using burst mode, making better use of the FIFO
+// Reads using burst mode (multiple I2C reads), making better use of the FIFO
+// Read data using HPS I2C0 controller
 void tru_adxl345_i2c_read_bm(void *buf, uint32_t len, uint32_t reg_addr_start){
 	uint8_t *buf8 = buf;
 	tru_hps_i2c_ic_data_cmd_var_t data_cmd = { .val = 0 };
@@ -95,7 +123,7 @@ void tru_adxl345_i2c_read_bm(void *buf, uint32_t len, uint32_t reg_addr_start){
 		// Note, we assume the TXFIFO and RXFIFO is empty
 		// The strategy is to make use of the FIFOs by filling them up first
 		// then read the data out in blocks.  Although not used here, it would
-		// benefit DMA transfers
+		// benefit with DMA transfers
 
 		// Calculate limit
 		txremain = (len < TRU_HPS_I2C_RXFIFO_DEPTH) ? len : TRU_HPS_I2C_RXFIFO_DEPTH;
@@ -120,6 +148,7 @@ void tru_adxl345_i2c_read_bm(void *buf, uint32_t len, uint32_t reg_addr_start){
 	}
 }
 
+// Read data using HPS I2C0 controller
 void tru_adxl345_i2c_read(void *buf, uint32_t len, uint32_t reg_addr_start){
 	uint8_t *buf8 = buf;
 	tru_hps_i2c_ic_data_cmd_var_t data_cmd = { .val = 0 };
@@ -151,6 +180,7 @@ void tru_adxl345_i2c_read(void *buf, uint32_t len, uint32_t reg_addr_start){
 	}
 }
 
+// Write data using HPS I2C0 controller
 void tru_adxl345_i2c_write(void *buf, uint32_t len, uint32_t reg_addr_start){
 	uint8_t *buf8 = buf;
 	tru_hps_i2c_ic_data_cmd_var_t data_cmd = { .val = 0 };
